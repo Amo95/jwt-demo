e605ad6ea9d172ac59cfb4768e53ab84381bdddf
amo95 <jameaamo@gmail.com>
2024-03-04 23:35:41 +0000

    rename controller

--- src/main/java/dev/jwtdemo/controller/AuthenticationController.java
+++ src/main/java/dev/jwtdemo/controller/AuthenticationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/test")
-public class HomeController {
+public class AuthenticationController {
 
     @GetMapping
     public String home() {
