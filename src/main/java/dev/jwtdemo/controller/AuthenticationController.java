941b8dc4596c68419aac0a88a9968f5262a8a0c6
amo95 <jameaamo@gmail.com>
2024-03-04 23:41:23 +0000

    added service class

--- src/main/java/dev/jwtdemo/controller/AuthenticationController.java
+++ src/main/java/dev/jwtdemo/controller/AuthenticationController.java
@@ -1,9 +1,9 @@
 package dev.jwtdemo.controller;
 
+import dev.jwtdemo.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
---
+++ src/main/java/dev/jwtdemo/service/AuthenticationService.java
@@ -0,0 +1,7 @@
+package dev.jwtdemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+}
